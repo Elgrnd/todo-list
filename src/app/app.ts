e4a4293d1719bc22df9efdf0c3d8933813cbc6e3
Galdric Desert-Caparros <galdric.desert-caparros@etu.umontpellier.fr>
2026-09-18 13:34:23 +0200

    feat : ajout filtre nom de tâche

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component} from '@angular/core';
 import { TodoItem } from './todo-item/todo-item';
 import { Task } from './task';
-import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, Validators, FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [TodoItem, ReactiveFormsModule],
+  imports: [TodoItem, ReactiveFormsModule, FormsModule],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
@@ -13,25 +13,26 @@ export class App {
   tasks: Task[] = [
     {
       id: 1,
-      title: "Apprendre Angular",
-      completed: false
+      title: 'Apprendre Angular',
+      completed: false,
     },
     {
       id: 2,
-      title: "Créer ma première To-Do List",
-      completed: false
+      title: 'Créer ma première To-Do List',
+      completed: false,
     },
     {
       id: 3,
-      title: "Comprendre les composants",
-      completed: false
-    }
+      title: 'Comprendre les composants',
+      completed: false,
+    },
   ];
 
-  newTask = new FormControl('');
+  newTask = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  searchControl = new FormControl('', {nonNullable: true});
 
   toogleTask(id: number) {
-    const task = this.tasks.find(task => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
 
     if (task) {
       task.completed = !task.completed;
@@ -39,23 +40,30 @@ export class App {
   }
 
   deleteTask(id: number) {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
   addTask() {
-    const title = this.newTask.value;
-
-    if (!title) {
+    if (this.newTask.invalid || !this.newTask.value) {
       return;
     }
+
+    const title = this.newTask.value.trim();
 
     const task: Task = {
       id: Date.now(),
       title: title,
-      completed: false
-    }
+      completed: false,
+    };
 
     this.tasks.push(task);
     this.newTask.reset();
+  }
+
+  get filteredTasks(): Task[] {
+    const search = this.searchControl.value.toLowerCase();
+
+    return this.tasks.filter(task =>
+    task.title.toLowerCase().includes(search))
   }
 }
